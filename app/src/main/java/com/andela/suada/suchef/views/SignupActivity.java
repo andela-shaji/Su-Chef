bd65a1b8e8166a177fb069290501df1aa05d508c
Suada Haji <suada.haji@andela.com>
2016-07-18 20:55:45 +0300

    Add content to the signup activity

---
+++ app/src/main/java/com/andela/suada/suchef/views/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.andela.suada.suchef.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.andela.suada.suchef.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
